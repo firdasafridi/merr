aece07ccd3f082228ca9b6f086aed487b4064602
firdasafridi <firdasafridi@ymail.com>
2020-10-23 15:40:59 +0700

    [DOC] change usefull to useful

--- doc.go
+++ doc.go
@@ -2,7 +2,7 @@
 // (see https://pkg.go.dev/github.com/firdasafridi/merr?tab=overview) for more documentation
 //
 // This allows a function ig Go to return an list of error.
-// It will be usefull when you do call some function to get multiple error handler.
+// It will be useful when you do call some function to get multiple error handler.
 //
 //
 // merr is fully compatible with the Go standard library errors package, including the functions As, Is, and Unwrap.
